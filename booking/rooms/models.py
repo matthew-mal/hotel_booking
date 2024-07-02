b2563e53b193bc22b4254863fa1098c4fda8316a
mat_mal <matvey_maljanov@rambler.ru>
2024-07-02 15:49:11 +0300

    final commit

--- booking/rooms/models.py
+++ booking/rooms/models.py
@@ -12,40 +12,53 @@ class MyUser(AbstractUser):
 
 
 class Room(models.Model):
-    STANDARD = 'standard'
-    DELUXE = 'deluxe'
-    SUITE = 'suite'
+    STANDARD = "standard"
+    DELUXE = "deluxe"
+    SUITE = "suite"
 
     ROOM_TYPE_CHOICES = [
-        (STANDARD, 'Standard'),
-        (DELUXE, 'Deluxe'),
-        (SUITE, 'Suite'),
+        (STANDARD, "Standard"),
+        (DELUXE, "Deluxe"),
+        (SUITE, "Suite"),
     ]
 
-    name = models.PositiveSmallIntegerField(verbose_name='Номер')
-    price_per_day = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за сутки')
-    capacity = models.IntegerField(verbose_name='Количество мест')
-    is_available = models.BooleanField(default=True)
-    room_type = models.CharField(max_length=10, choices=ROOM_TYPE_CHOICES, default=STANDARD)
+    name = models.PositiveSmallIntegerField(verbose_name="Room")
+    price_per_day = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Price per day"
+    )
+    capacity = models.IntegerField(verbose_name="Capacity")
+    is_available = models.BooleanField(default=True, verbose_name="Available?")
+    room_type = models.CharField(
+        max_length=10,
+        choices=ROOM_TYPE_CHOICES,
+        default=STANDARD,
+        verbose_name="Room Type",
+    )
 
     def __str__(self):
-        return f'{self.name} - {self.room_type}'
+        return f"{self.name} - {self.room_type}"
 
 
 class Booking(models.Model):
-    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, verbose_name="User")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, verbose_name="Room")
+    start_date = models.DateField(verbose_name="Start date")
+    end_date = models.DateField(verbose_name="End date")
+    cost = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        verbose_name="Total price",
+    )
     canceled = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.room.name} - {self.user.username} ({self.start_date} to {self.end_date})'
+        return f"{self.room.name} - {self.user.username} ({self.start_date} to {self.end_date})"
 
     def calculate_cost(self):
-        start_date = datetime.strptime(self.start_date, '%Y-%m-%d').date()
-        end_date = datetime.strptime(self.end_date, '%Y-%m-%d').date()
+        start_date = datetime.strptime(self.start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(self.end_date, "%Y-%m-%d").date()
         duration = (end_date - start_date).days
         self.cost = duration * self.room.price_per_day
 
