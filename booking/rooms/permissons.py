127b1c68780400a0212a8c0cc374f39f32ad4572
mat_mal <matvey_maljanov@rambler.ru>
2024-07-02 13:48:00 +0300

    refactor roomsviewset +permission

--- booking/rooms/permissons.py
+++ booking/rooms/permissons.py
@@ -1,9 +1,21 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrStaff(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
-            request.method in SAFE_METHODS or
-            request.user.is_authenticated and (obj.user == request.user or request.user.is_staff)
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+            and (obj.user == request.user or request.user.is_staff)
         )
+
+
+class AdminOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        # Разрешаем GET запросы всем
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Проверяем, является ли пользователь администратором
+        return request.user and request.user.is_superuser
--- booking/rooms/views.py
+++ booking/rooms/views.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from .filters import RoomFilter
-from .models import Room, Booking, MyUser
-from .permissons import IsOwnerOrStaff
-from .serializers import RoomSerializer, BookingSerializer, MyUserSerializer
+from .models import Booking, MyUser, Room
+from .permissons import AdminOnlyPermission, IsOwnerOrStaff
+from .serializers import BookingSerializer, MyUserSerializer, RoomSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -29,7 +29,9 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
 
 class RoomViewSet(viewsets.ModelViewSet):
@@ -38,28 +40,10 @@ class RoomViewSet(viewsets.ModelViewSet):
 
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = RoomFilter
-    filterset_fields = ['price_per_day', 'capacity']
-    ordering_fields = ['price_per_day', 'capacity']
+    filterset_fields = ["price_per_day", "capacity"]
+    ordering_fields = ["price_per_day", "capacity"]
 
-    permission_classes = [AllowAny]
-
-    def create(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return Response({'error': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def destroy(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return Response({'error': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = [AdminOnlyPermission]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
@@ -75,32 +59,34 @@ class BookingViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return Response({'error': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Authentication required"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
 
-        room_id = request.data.get('room')
-        start_date = request.data.get('start_date')
-        end_date = request.data.get('end_date')
+        room_id = request.data.get("room")
+        start_date = request.data.get("start_date")
+        end_date = request.data.get("end_date")
 
         try:
             room = Room.objects.get(id=room_id)
         except Room.DoesNotExist:
-            return Response({'error': 'Room does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Room does not exist"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         conflicting_bookings = Booking.objects.filter(
-            room=room,
-            start_date__lt=end_date,
-            end_date__gt=start_date
+            room=room, start_date__lt=end_date, end_date__gt=start_date
         )
 
         if conflicting_bookings.exists():
-            return Response({'error': 'Room is already booked for the specified dates'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Room is already booked for the specified dates"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         booking = Booking.objects.create(
-            user=request.user,
-            room=room,
-            start_date=start_date,
-            end_date=end_date
+            user=request.user, room=room, start_date=start_date, end_date=end_date
         )
 
         room.is_available = False
@@ -110,17 +96,22 @@ class BookingViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=["post"])
     def cancel_booking(self, request, pk=None):
         booking = Booking.objects.get(pk=pk)
         if booking.canceled:
-            return Response({'error': 'Booking already canceled'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Booking already canceled"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         today = datetime.now().date()
 
         if booking.start_date <= today:
-            return Response({'error': 'Cannot cancel booking if it has already started'},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Cannot cancel booking if it has already started"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         booking.canceled = True
         booking.save()
@@ -130,11 +121,17 @@ class BookingViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(booking)
 
-        return Response(f'Booking {serializer.data} was successfully canceled', status=status.HTTP_200_OK)
+        return Response(
+            f"Booking {serializer.data} was successfully canceled",
+            status=status.HTTP_200_OK,
+        )
 
     def destroy(self, request, *args, **kwargs):
         if not request.user.is_staff:
-            return Response({'error': 'Authentication required'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Authentication required"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
 
         instance = self.get_object()
         self.perform_destroy(instance)
