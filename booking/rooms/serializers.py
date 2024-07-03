1429f529c5c072723b79a515d30e1e8f0ec991de
mat_mal <matvey_maljanov@rambler.ru>
2024-07-03 10:06:45 +0300

    a little query optimization

--- booking/rooms/serializers.py
+++ booking/rooms/serializers.py
@@ -1,18 +1,19 @@
 from rest_framework import serializers
-from .models import Room, Booking, MyUser
+
+from .models import Booking, MyUser, Room
 
 
 class MyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
-        fields = ('username', 'password', 'email')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ("username", "password", "email")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = MyUser.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data['email']
+            username=validated_data["username"],
+            password=validated_data["password"],
+            email=validated_data["email"],
         )
         return user
 
@@ -20,10 +21,20 @@ class MyUserSerializer(serializers.ModelSerializer):
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('id', 'name', 'price_per_day', 'capacity', 'room_type', 'is_available')
+        fields = (
+            "id",
+            "name",
+            "price_per_day",
+            "capacity",
+            "room_type",
+            "is_available",
+        )
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username")
+    room_name = serializers.IntegerField(source="room.name")
+
     class Meta:
         model = Booking
-        fields = ('user', 'room', 'start_date', 'end_date', 'cost', 'canceled')
+        fields = ("username", "room_name", "start_date", "end_date", "cost", "canceled")
